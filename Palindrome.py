f30dce21f726483fe04895557120f0f66c21f22f
Tech-Learner07 <74245472+Tech-Learner07@users.noreply.github.com>
2021-01-30 11:16:22 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -4,6 +4,6 @@ def isPalindrome(word):
   
   return word == word[::-1] # [::-1] is the method to reverse a list or a word
 
-isPalindrome(Level) # returns True
+isPalindrome(LevEl) # returns True
 isPalindrome(malayalam) # returns True
 isPalindrome(python) # returns False
