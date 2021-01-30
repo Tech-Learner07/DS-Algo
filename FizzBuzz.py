631c3e11145bbd7d0df2cd37c932ded5038ee766
Tech-Learner07 <74245472+Tech-Learner07@users.noreply.github.com>
2021-01-30 11:00:51 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,32 @@
+'''
+Question:
 
+Write a program to print numbers from 1 to 100.
+For the multiples of 3 print 'Fizz', for the 
+multiples of 5 print 'Buzz' and both multiples
+3 & 5 print 'FizzBuzz'.
+
+Key:
+Knowing the modulo(%) operator that is
+if n % 5 == 0
+if n % 3 == 0
+if n % 3 and n % 5 == 0
+
+Solution:
+'''
+def fizzBuzz():
+  # printing numbers from 1 to 100
+  for n in range(1, 101):
+    # if number is multiple of 3 and 5
+    if n % 3 == n % 5 == 0:
+      print("FizzBuzz")
+    elif n % 3 == 0:
+    # if number is multiple of 3
+      print("Fizz")
+    elif n % 5 == 0:
+    # if number is multiple of 5
+      print("Buzz")
+    else:
+      print(i)
+
+fizzBuzz()
