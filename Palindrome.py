a4dd731c5e2ab61c9ed89ad19964829ba4294337
Tech-Learner07 <74245472+Tech-Learner07@users.noreply.github.com>
2021-01-30 11:15:54 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,1 +1,9 @@
 
+def isPalindrome(word):
+  word = word.lower() # OR you can use word.upper() it doesn't matter
+  
+  return word == word[::-1] # [::-1] is the method to reverse a list or a word
+
+isPalindrome(Level) # returns True
+isPalindrome(malayalam) # returns True
+isPalindrome(python) # returns False
