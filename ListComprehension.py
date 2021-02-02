fa139575d58b4f76ca36691ba181d6cf70a776be
MR-Hack-403 <72344536+MR-Hack-403@users.noreply.github.com>
2021-02-02 16:48:36 +0530

    Update ListComprehension.py

--- ListComprehension.py
+++ ListComprehension.py
@@ -12,6 +12,12 @@ Solution:
 
 
 def digits(string):
+    # In list comprehension you can write the code in only on line.
+    # Step 1: expression here char
+    # Step 2: iteration
+    # Step 3: filtering.
+    # Eg: [expression(i) for i in list if filter(i)]
+    # here [-1] is used to identify the right most digit in the string.
     return [char for char in string if char.isdigit()][-1]
 
 
