3308d683ebf3240dc1983de35a93725e747eedf4
MR-Hack-403 <72344536+MR-Hack-403@users.noreply.github.com>
2021-02-02 16:04:42 +0530

    create Duplicates.py

--- Duplicates.py
+++ Duplicates.py
@@ -1,15 +1,26 @@
 '''
 Find the duplicates in a set.
+
+Key:
+Identifying the use of set() function
 '''
 
 
 def duplicate(theList):
+    # Empty list to add duplicates
     dup = []
+    # Empty set for to check for duplicates
     theSet = set()
     for item in theList:
+        # Get the length of the set
         lengthBefore = len(theSet)
+        # Attempt to add item to the set
         theSet.add(item)
+        # Check the new length of the set
+        # Duplicates cannot be add to a set
         lengthAfter = len(theSet)
+
+        # If the length doesn't changed the item is a duplicate
         if lengthAfter == lengthBefore:
             dup.append(item)
     return dup
